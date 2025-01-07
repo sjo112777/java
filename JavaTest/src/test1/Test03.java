c94cda20313905ca23ca3db57da5dea207e33197
sjo112777 <sjo112777@gmail.com>
2025-01-07 12:13:07 +0900

    add JavaTest git study

---
+++ JavaTest/src/test1/Test03.java
@@ -0,0 +1,29 @@
+package test1;
+/*
+ * 날짜: 2025/01/03
+ * 이름: 손준오
+ * 내용: 자바 연산자 연습문제
+ */
+public class Test03 {
+
+	public static void main(String[] args) {
+		
+		int num = 1;
+		int result = 0;
+		
+		result = num++;
+		System.out.println("result" + result);
+		
+		result = ++num;
+		System.out.println("result" + result);
+		
+		result = num--;
+		System.out.println("result" + result);
+		
+		result = --num;
+		System.out.println("result" + result);
+		
+		
+	}
+	
+}
---
+++ JavaTest/src/test1/Test04.java
@@ -0,0 +1,20 @@
+package test1;
+
+import java.util.Scanner;
+
+public class Test04 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("점수 입력: ");
+		int score = sc.nextInt();
+		char grade;
+		
+		System.out.println("입력한 점수는" +score + "입니다.");
+		
+		if(score >= 90 )
+	}
+
+}
