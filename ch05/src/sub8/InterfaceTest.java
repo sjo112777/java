1fe1e0d7b518f46793bf214d04321b9d273cce95
sjo112777 <sjo112777@gmail.com>
2025-01-10 15:33:02 +0900

    add ch05

---
+++ ch05/src/sub8/InterfaceTest.java
@@ -0,0 +1,45 @@
+package sub8;
+
+/*
+ * 날짜: 2025/01/10
+ * 이름: 손준오
+ * 내용: Java 인터페이스 실습하기
+ */
+public class InterfaceTest {
+
+	public static void main(String[] args) {
+		
+		// 인터페이스 예제1 - 표준화된 공통 클래스 구조 설계
+		RemoteControl lg = new RemoteLG();
+		RemoteControl samsung = new RemoteSamsung();
+		
+		lg.powerOn();
+		lg.chDown();
+		lg.soundUp();
+		lg.powerOff();
+		
+		samsung.powerOn();
+		samsung.chUp();
+		samsung.soundDown();
+		samsung.powerOff();
+		
+		
+		// 인터페이스 예제2 - 유연성(다형성을 통한 결합도 완화)
+		Bulb bulb = new Bulb();
+		Socket socket = new Cable(bulb);
+		
+		socket.switchOn();
+		socket.switchOff();
+		
+		
+		// 인터페이스 예제3 - 다중 상속 효과
+		SmartTv stv = new SmartTv();
+		stv.process();
+		
+		//추상클래스보다는 인터페이스를 많이 사용
+		
+		
+	}
+	
+	
+}
