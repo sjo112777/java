ea0c23f74305dc12b28c904304649520854ac067
sjo112777 <sjo112777@gmail.com>
2025-01-10 10:44:52 +0900

    add JavaTest git study

--- JavaTest/src/test1/Test06.java
+++ JavaTest/src/test1/Test06.java
@@ -1,5 +1,21 @@
 package test1;
 
-public class Test06 {
+import java.util.Scanner;
 
+public class Test06 {
+public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("x값 입력 : ");
+		int x = sc.nextInt();
+		
+		System.out.print("y값 입력 : ");
+		int y = sc.nextInt();
+		
+		String result = x > y ? "x가 더 큽니다." : "y가 더 큽니다.";
+		
+		System.out.println(result);
+	}
 }
+
